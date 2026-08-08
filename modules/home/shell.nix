a8af41c2a6aff8b01e28fc681cfdfd5420f8b5fb
r3quisitevariety <x3roo@proton.me>
2026-08-08 16:55:06 -0400

    bunnyfetch alias

--- modules/home/shell.nix
+++ modules/home/shell.nix
@@ -27,6 +27,7 @@
       less = "moor";
       ns = "nix-search-tv print | fzf --preview 'nix-search-tv preview {}' --scheme history";
       t = "tmux";
+      bunnyfetch = "fastfetch";
     };
     bashrcExtra = ''
       PS1='\[\e[1;32m\]\u@\h\[\e[0m\]:\[\e[1;34m\]\w\[\e[0m\]\$ '
