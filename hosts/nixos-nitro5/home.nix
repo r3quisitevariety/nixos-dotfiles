3e8bf0ca6e3657b60e1fad303c741030725e4179
r3quisitevariety <x3roo@proton.me>
2026-08-09 17:20:55 -0400

    add xdg.mimeApps config

--- hosts/nixos-nitro5/home.nix
+++ hosts/nixos-nitro5/home.nix
@@ -128,6 +128,50 @@
     hyprcursor.size = 16;
   };
 
+  # thanks elias
+  xdg.mimeApps = {
+    enable = true;
+    defaultApplications = let
+      imageViewer = "imv.desktop";
+      videoViewer = "mpv.desktop";
+      audioViewer = "mpv.desktop";
+      fileViewer = "org.gnome.Nautilus.desktop";
+      browser = "firefox.desktop";
+      textEditor = "codium.desktop";
+      chromium = "chromium.desktop";
+    in {
+      "image/jpeg" = imageViewer;
+      "image/png" = imageViewer;
+      "image/gif" = browser;
+      "image/webp" = imageViewer;
+      "image/heif" = imageViewer;
+      "text/plain" = textEditor;
+      "text/css" = textEditor;
+      "application/x-shellscript" = textEditor;
+      "application/x-zerosize" = textEditor;
+      "text/html" = browser;
+      "x-scheme-handler/http" = browser;
+      "x-scheme-handler/https" = browser;
+      "application/pdf" = browser;
+      "application/vnd.openxmlformats-officedocument.wordprocessingml.document" = chromium;
+      "audio/mpeg" = audioViewer;
+      "inode/directory" = fileViewer;
+      "video/mp4" = videoViewer;
+      "video/x-matroska" = videoViewer;
+      "video/webm" = videoViewer;
+      "video/ogg" = videoViewer;
+      "video/quicktime" = videoViewer;
+      "video/x-flv" = videoViewer;
+      "video/x-msvideo" = videoViewer;
+      "video/x-ms-wmv" = videoViewer;
+      "video/mpeg" = videoViewer;
+      "text/x-chdr" = textEditor;
+      "text/x-csrc" = textEditor;
+      "text/x-c++hdr" = textEditor;
+      "text/x-c++src" = textEditor;
+    };
+  };
+
   imports = [
     # TODO i commented these out because i have yet to resolve hostname hardcoding for modules
     ../../modules/home/neovim.nix
