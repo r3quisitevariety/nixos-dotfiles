617b7b66b80947a94d6f194b5e9a39afa7ea904d
r3quisitevariety <x3roo@proton.me>
2026-08-14 10:36:17 -0400

    disable gn*me

--- hosts/nixos-inspiron/configuration.nix
+++ hosts/nixos-inspiron/configuration.nix
@@ -53,11 +53,11 @@
   };
 
   # Enable the X11 windowing system.
-  services.xserver.enable = true;
+  services.xserver.enable = false;
 
   # Enable the GNOME Desktop Environment.
-  services.xserver.displayManager.gdm.enable = true;
-  services.xserver.desktopManager.gnome.enable = true;
+  services.xserver.displayManager.gdm.enable = false;
+  services.xserver.desktopManager.gnome.enable = false;
 
   # Configure keymap in X11
   services.xserver.xkb = {
