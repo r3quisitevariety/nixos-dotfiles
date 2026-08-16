d058299ff83894261e3f16ae8e33fce139afe4d7
r3quisitevariety <x3roo@proton.me>
2026-08-16 13:21:34 -0400

    oops forget something

--- modules/nixos/nix.nix
+++ modules/nixos/nix.nix
@@ -5,13 +5,4 @@
     "nix-command"
     "flakes"
   ];
-
-  nix.settings = {
-    extra-substituters = [
-      "https://noctalia.cachix.org"
-    ];
-    extra-trusted-public-keys = [
-      "noctalia.cachix.org-1:pCOR47nnMEo5thcxNDtzWpOxNFQsBRglJzxWPp3dkU4="
-    ];
-  };
 }
