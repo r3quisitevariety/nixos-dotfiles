49790b9a8e6122f44817c26a3a16ddb75e75af2e
r3quisitevariety <x3roo@proton.me>
2026-08-16 02:08:40 -0400

    make kitty config better

--- hosts/nixos-nitro5/home.nix
+++ hosts/nixos-nitro5/home.nix
@@ -13,16 +13,23 @@
     };
 
     settings = {
+      enable_audio_bell = false;
       allow_remote_control = "yes";
+      listen_on = "unix:/tmp/kitty";
       background_blur = 24;
       background_opacity = "0.8";
       confirm_os_window_close = 0;
-      cursor_trail = 5;
       hide_window_decorations = "yes";
       placement_strategy = "top-left";
+      cursor_trail = 4;
+      cursor_trail_decay = "0.1 0.5";
+      cursor_trail_start_threshold = 0;
       resize_debounce_time = "0 0";
       scrollbar = "never";
       window_padding_width = 2;
+      bold_font = "auto";
+      italic_font = "auto";
+      bold_italic_font = "auto";
     };
 
     keybindings = {
