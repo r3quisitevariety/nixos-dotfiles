ac37db445c493b6d9244977556b281de9b86507f
r3quisitevariety <x3roo@proton.me>
2026-08-17 04:20:34 -0400

    add zed

--- hosts/nitro5/home.nix
+++ hosts/nitro5/home.nix
@@ -121,6 +121,7 @@
   imports = [
     # TODO i commented these out because i have yet to resolve hostname hardcoding for modules
     ./syncthing.nix
+    ./zed.nix
     ../../modules/home/neovim.nix
     ../../modules/home/shell.nix
     ../../modules/home/music.nix
