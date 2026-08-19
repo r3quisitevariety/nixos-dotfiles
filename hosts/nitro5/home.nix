c050b398883785d9df304edafbcf29fb34b285c1
r3quisitevariety <x3roo@proton.me>
2026-08-18 21:26:28 -0400

    remove elias's zed, and stellas too

--- hosts/nitro5/home.nix
+++ hosts/nitro5/home.nix
@@ -125,7 +125,6 @@
   imports = [
     # TODO i commented these out because i have yet to resolve hostname hardcoding for modules
     ./syncthing.nix
-    ../../modules/home/zedlias.nix
     ../../modules/home/neovim.nix
     ../../modules/home/shell.nix
     ../../modules/home/music.nix
