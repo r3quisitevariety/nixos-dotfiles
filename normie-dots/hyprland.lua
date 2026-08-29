579ccbf0d47b4a853d15e6064aaf0167916f41a8
r3quisitevariety <x3roo@proton.me>
2026-08-29 13:50:25 -0400

    add nvidia envs to hypr

--- normie-dots/hyprland.lua
+++ normie-dots/hyprland.lua
@@ -48,10 +48,10 @@ end)
 hl.env("QT_QPA_PLATFORMTHEME", "qt6ct")
 
 -- NVIDIA - disabled, causes issues
---hl.env("GBM_BACKEND", "nvidia-drm")
---hl.env("__GLX_VENDOR_LIBRARY_NAME", "nvidia")
---hl.env("LIBVA_DRIVER_NAME", "nvidia")
---hl.env("NVD_BACKEND", "direct")
+hl.env("GBM_BACKEND", "nvidia-drm")
+hl.env("__GLX_VENDOR_LIBRARY_NAME", "nvidia")
+hl.env("LIBVA_DRIVER_NAME", "nvidia")
+hl.env("NVD_BACKEND", "direct")
 
 -- XDG / Portal
 hl.env("XDG_CURRENT_DESKTOP", "Hyprland")
