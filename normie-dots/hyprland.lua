ee5e79b397d781577137d5e8a789b6499d6a621c
r3quisitevariety <x3roo@proton.me>
2026-09-04 16:51:37 -0400

    chore(hyprland): disable unecessary env vars

--- normie-dots/hyprland.lua
+++ normie-dots/hyprland.lua
@@ -49,15 +49,15 @@ end)
 hl.env("QT_QPA_PLATFORMTHEME", "qt6ct")
 
 -- NVIDIA - disabled, causes issues
-hl.env("GBM_BACKEND", "nvidia-drm")
-hl.env("__GLX_VENDOR_LIBRARY_NAME", "nvidia")
-hl.env("LIBVA_DRIVER_NAME", "nvidia")
-hl.env("NVD_BACKEND", "direct")
+--hl.env("GBM_BACKEND", "nvidia-drm")
+--hl.env("__GLX_VENDOR_LIBRARY_NAME", "nvidia")
+--hl.env("LIBVA_DRIVER_NAME", "nvidia")
+--hl.env("NVD_BACKEND", "direct")
 
 -- XDG / Portal
-hl.env("XDG_CURRENT_DESKTOP", "Hyprland")
-hl.env("XDG_SESSION_TYPE", "wayland")
-hl.env("XDG_SESSION_DESKTOP", "Hyprland")
+--hl.env("XDG_CURRENT_DESKTOP", "Hyprland")
+--hl.env("XDG_SESSION_TYPE", "wayland")
+--hl.env("XDG_SESSION_DESKTOP", "Hyprland")
 
 -----------------------
 ---- LOOK AND FEEL ----
